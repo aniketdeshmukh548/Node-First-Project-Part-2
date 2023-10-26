fa62a0423775ed7361d027fcfcf72ee5f6e1bcec
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-10-26 10:21:27 +0530

    Dynamic Routes Part 3 (Query Params)

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,19 +28,40 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getcart(cart=>{
+    Product.fetchAll(products=>{
+      const cartProducts=[]
+      for(product of products){
+        const cartProductsData=cart.products.find(prod=>prod.id===product.id)
+        if(cartProductsData){
+          cartProducts.push({productData:product,qty:cartProductsData.qty})
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:cartProducts
+      });
+    })
+  })
 };
 
 exports.postCart=(req,res,next)=>{
   const prodId=req.body.productId;
-  Product.findId(prodId,(product)=>{
+  Product.findId(prodId,product=>{
     Cart.addProduct(prodId,product.price)
   })
   console.log(prodId);
   res.redirect('/cart')
+}
+
+exports.postCartDeleteProduct=(req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.findId(prodId,product=>{
+    Cart.deleteProduct(prodId,product.price);
+    res.redirect('/cart')
+  })
+  
 }
 
 exports.getOrders = (req, res, next) => {
@@ -56,3 +77,4 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
